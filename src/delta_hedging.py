086e461745fea45c2bf7bf337c70513e6dd85592
Juan Ignacio Diaz <juandiaz02@mi.unc.edu.ar>
2025-11-06 15:57:14 -0300

    REFACTOR: delta now is calculated using montecarlo

--- src/delta_hedging.py
+++ src/delta_hedging.py
@@ -1,14 +1,26 @@
 import pandas as pd
+from random import random
 import numpy as np
-from math import erf
 from price_simulation import sim_stock_price
 
 
 def get_d1(S_0, K, r, T, deviation):
     return (np.log(S_0 / K) + (r +(deviation ** 2 / 2)) * T) / (deviation * np.sqrt(T))
 
+##Estima la integral de funciong entre -inf y a con 5000 simulaciones
+def monte_carlo_inf_a(g, a, n_sim):
+    suma = 0
+    for _ in range(n_sim):
+        u = random()
+        suma += g(a - (1/u - 1)) / (u**2)
+    return suma / n_sim
+
+def funciong(x):
+    return np.exp((-x**2)/2)
+
 def norm_cdf(x):
-    return 0.5 * (1.0 + erf(x / np.sqrt(2.0)))
+    estim_monte_carlo = monte_carlo_inf_a(funciong, x, n_sim=1000)
+    return 1 / np.sqrt(2 * np.pi) * estim_monte_carlo
 
 def delta_call_long(S0, K, r, T, deviation):
     x = get_d1(S0, K, r, T, deviation)
@@ -16,7 +28,7 @@ def delta_call_long(S0, K, r, T, deviation):
 
 def delta_put_long(S0, K, r, T, deviation):
     x = get_d1(S0, K, r, T, deviation)
-    return norm_cdf(x) - 1.0
+    return norm_cdf(x) - 1
 
 def main():
     K = 50
