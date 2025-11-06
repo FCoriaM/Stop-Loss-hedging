28a9b2636e507c3ef974626ea5223c2571016946
FCoriaM <fcoriameg@gmail.com>
2025-11-06 17:50:31 -0300

    add: delta_hedging_single_sim

--- src/delta_hedging.py
+++ src/delta_hedging.py
@@ -30,6 +30,37 @@ def delta_put(S0, K, r, tao, sigma):
     x = get_d1(S0, K, r, tao, sigma)
     return norm_cdf(x) - 1
 
+def delta_hedging_single_sim(S0, K, Delta0, r, sigma, T, amt_options, n_steps):
+    """
+    t | S(t) | Delta | Acciones compradas | Costo acciones compradas | Costo acumulado | Costo de Interes  
+    """
+    columns = ['t', 'S(t)', 'Delta', 'Acciones compradas', 'Costo acciones compradas', 'Costo acumulado', 'Costo de Interes']
+    df = pd.DataFrame(columns=columns)
+    dt = T / n_steps
+    S = np.empty(n_steps + 1, dtype=float)
+    Deltas = np.empty(n_steps + 1, dtype=float)
+    shares_purchased = np.empty(n_steps + 1, dtype=float)
+    cost_s_p = np.empty(n_steps + 1, dtype=float)
+    cum_cost = np.empty(n_steps + 1, dtype=float)
+    interest_cost = np.empty(n_steps + 1, dtype=float)
+
+    S[0] = S0
+    Deltas[0] = Delta0
+    shares_purchased[0] = Deltas[0] * amt_options 
+    cost_s_p[0] = shares_purchased[0] * S[0] / 1000 # in millions
+    cum_cost[0] = cost_s_p[0]
+    interest_cost[0] = 0
+
+    for k in range(1, n_steps + 1):
+        S[k] = sim_stock_price(r, sigma, S[k-1], dt)
+        tao = T - k*dt
+        Deltas[k] = delta_call(S0, K, r, tao, sigma)
+        shares_purchased[k] = (Deltas[k] - Deltas[k-1]) * amt_options
+        cost_s_p[k] = shares_purchased[k] * S[k] / 1000 # in millions
+        cum_cost[k] = cost_s_p[k-1] * (1 + r * dt) + cost_s_p[k]
+        interest_cost[k] = cum_cost[k-1] * r * dt
+    return df
+
 def main():
     K = 50
     S_0 = 49
@@ -43,7 +74,6 @@ def main():
 
     Delta = delta_call_long(S_0, K, r, T, deviation)
 
-    print(f"d1: {d1}")
     print(f"N(d1) (Delta call): {Delta}")
 
 if __name__ == '__main__':
